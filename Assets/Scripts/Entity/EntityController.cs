9e5cfece126c2aeabc43905a29f1a99169aea5af
AlbertVVila <albertvalvila@gmail.com>
2020-08-22 00:04:19 +0200

    Refactor entity into person and zombie controllers

---
+++ Assets/Scripts/Entity/EntityController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public abstract class EntityController : MonoBehaviour
+{
+    public Color entityHPColor;
+    public Slider healthBar;
+    public int health = 100;
+
+    // TODO: Update health bar when health value is affected
+
+    protected bool deathIdle; // This will avoid multiple calls to the coroutine
+    protected NavMeshAgent navMeshAgent;
+
+    protected GameController gameController = null;
+
+    private void Awake()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController")?.GetComponent<GameController>();
+        gameController.OnGameCreated.Subscribe(OnGameCreated);
+
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    protected virtual void OnGameCreated()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar.value = health;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        healthBar.value = health;
+    }
+
+    IEnumerator Death()
+    {
+        //TODO: trigger death animation
+        yield return new WaitForSeconds(GameController.transformTime);
+
+        DeathBehaviour();
+        deathIdle = false;
+    }
+
+    protected abstract void DeathBehaviour();
+}
---
+++ Assets/Scripts/Entity/ZombieController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : EntityController
+{
+    private float timeZombieBetweenChecks = 0.5f;
+    private float zombieCheckTimer;
+
+    protected override void OnGameCreated()
+    {
+        base.OnGameCreated();
+        gameController.AddZombie(this);
+        navMeshAgent.speed = GameController.zombieSpeed;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        //Basic zombie behaviour
+        if (timeZombieBetweenChecks < zombieCheckTimer)
+        {
+            zombieCheckTimer = 0.0f;
+            SearchForNewVictim();
+        }
+        else
+        {
+            zombieCheckTimer += Time.deltaTime;
+        }
+    }
+
+    private void SearchForNewVictim()
+    {
+        GameObject target = null;
+        EntityController targetController = null;
+        float minDistance = 10000000; //needs to be BIG the first time
+        foreach (var person in gameController.people)
+        {
+            var distance = Vector3.Distance(person.gameObject.transform.position, transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                target = person.gameObject;
+                targetController = person;
+            }
+        }
+        if (target != null)
+        {
+            navMeshAgent.SetDestination(target.transform.position);
+            if (minDistance < GameController.zombieAttackDistance)
+            {
+                zombieCheckTimer = -2.0f; //this way they remain in place
+                targetController.TakeDamage(GameController.zombieAttackDamage);
+                navMeshAgent.ResetPath();
+            }
+        }
+    }
+
+    protected override void DeathBehaviour()
+    {
+        //this has to happen every 1 sec or so better than in any frame
+    }
+}
