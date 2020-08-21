c19aed49c77c3a4a76b8f9b76c62dc7b5e1fcf24
Alfonso <alfonsomestres@gmail.com>
2020-08-21 21:22:24 +0200

    Controller last update

--- Assets/Scripts/CivilianController.cs
+++ Assets/Scripts/CivilianController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
 public class CivilianController : MonoBehaviour
@@ -7,17 +8,20 @@ public class CivilianController : MonoBehaviour
     public int health = 100;
     public bool zombie;
 
+    private bool deathIdle; // This will avoid multiple calls to the coroutine
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameController.peopleAlive = GameController.peopleAlive + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!zombie && health <= 100)
+        if(!deathIdle && health <= 0 )
         {
+            deathIdle = true;
             StartCoroutine("Death");
         }
     }
@@ -25,16 +29,27 @@ public class CivilianController : MonoBehaviour
     IEnumerator Death()
     {
         //TODO: trigger death animation
-        yield return new WaitForSeconds(4f);
+        yield return new WaitForSeconds(GameController.transformTime);
 
-        zombie = Random.Range(0.0f, 100.0f) <= GameController.zombieProbability;
         if (zombie)
         {
-            health = GameController.zombieHealth;
-            //TODO:  trigger effect 
-            //TODO:  change skin change skin
+            // TODO: Disable collider and scripts.
+        } 
+        else
+        {
+            // If the guy is not a zombie, take the chance to transform it
+            zombie = Random.Range(0.0f, 100.0f) <= GameController.zombieProbability;
+            if (zombie)
+            {
+                GameController.zombiesAlive = GameController.zombiesAlive + 1;
+                health = GameController.zombieHealth;
+                //TODO:  trigger effect 
+                //TODO:  change skin change skin
+            }
+
         }
 
+        deathIdle = false;
 
     }
 
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public int peopleAlive = 0;
-    public int zombiesAlive = 0;
+    public static float transformTime = 3.5f; // Time a person wait to be transform or killed
 
+    public static int peopleAlive = 0;
+    public static int zombiesAlive = 0;
     public static int zombieProbability = 10;
     public static int zombieHealth = 40;
+    public static float zombieSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        peopleAlive = GameObject.Find("Civilians").transform.childCount;
+
     }
 
     // Update is called once per frame
