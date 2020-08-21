ac0b4b3de2309e1e3a7019585cd28c43e0b3467d
Alfonso <alfonsomestres@gmail.com>
2020-08-21 20:59:14 +0200

    BasicScripting

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public int peopleAlive = 0;
+    public int zombiesAlive = 0;
+
     public static int zombieProbability = 10;
     public static int zombieHealth = 40;
 
@@ -22,6 +24,13 @@ public class GameController : MonoBehaviour
             //TODO: Win condition
             //TODO: trigger a couple of seconds to see everyone dead or transformed
             //TODO: trigger UI WIN and Rewards
+        }
+
+        if(zombiesAlive == 0)
+        {
+            //TODO: Lose condition
+            //TODO: trigger a couple of seconds to see everyone alive
+            //TODO: trigger UI LOSE and Rewards
         }
     }
 
