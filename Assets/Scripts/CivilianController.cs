7a6258ce6ccbca7b39354a9cc2db4f18d5f632e5
Alfonso <alfonsomestres@gmail.com>
2020-08-21 20:46:13 +0200

    GameController and CivilianController added

---
+++ Assets/Scripts/CivilianController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CivilianController : MonoBehaviour
+{
+    public int health = 100;
+    public bool zombie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!zombie && health <= 100)
+        {
+            StartCoroutine("Death");
+        }
+    }
+
+    IEnumerator Death()
+    {
+        //TODO: trigger death animation
+        yield return new WaitForSeconds(4f);
+
+        zombie = Random.Range(0.0f, 100.0f) <= GameController.zombieProbability;
+        if (zombie)
+        {
+            health = GameController.zombieHealth;
+            //TODO:  trigger effect 
+            //TODO:  change skin change skin
+        }
+
+
+    }
+
+}
+
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int peopleAlive = 0;
+    public static int zombieProbability = 10;
+    public static int zombieHealth = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        peopleAlive = GameObject.Find("Civilians").transform.childCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (peopleAlive == 0)
+        {
+            //TODO: Win condition
+            //TODO: trigger a couple of seconds to see everyone dead or transformed
+            //TODO: trigger UI WIN and Rewards
+        }
+    }
+
+
+}
