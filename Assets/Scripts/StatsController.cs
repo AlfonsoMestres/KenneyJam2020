554213495d70873f41254938f734dd71f41da684
Alfonso <alfonsomestres@gmail.com>
2020-08-22 18:53:15 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/StatsController.cs
+++ Assets/Scripts/StatsController.cs
@@ -5,29 +5,49 @@ using UnityEngine.UI;
 
 public class StatsController : MonoBehaviour
 {
+    public List<int> prices;
 
+    private Text maxedText;
+    private Button buyButton;
     private Slider sliderStat;
+    private Text buyAmount;
     private int statPrice;
 
     // Start is called before the first frame update
     void Start()
     {
+        buyButton = gameObject.GetComponentInChildren<Button>();
         sliderStat = gameObject.GetComponentInChildren<Slider>();
-        //statPrice = int.Parse(gameObject.transform.Find("Amount").GetComponent<Text>().text);
+        maxedText = gameObject.transform.Find("MaxedStat").GetComponent<Text>();
+        sliderStat.maxValue = prices.Count;
+        buyAmount = gameObject.transform.Find("Amount").GetComponent<Text>();
+        buyAmount.text = prices[0].ToString();
     }
 
     public void IncreaseStat()
     {
         sliderStat.value = sliderStat.value + 1;
+        if (sliderStat.value == sliderStat.maxValue)
+        {
+            buyAmount.gameObject.SetActive(false);
+            buyButton.gameObject.SetActive(false);
+            maxedText.enabled = true;
+
+        }
+        statPrice = prices[(int)sliderStat.value];
+        gameObject.transform.Find("Amount").GetComponent<Text>().text = statPrice.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // TODO: Disable the button if the amount is not enough
-        if (GameController.cursedHeartsObtained >= statPrice)
+        if (GameController.cursedHeartsObtained < statPrice)
         {
-
+            buyButton.enabled = false;
+        } 
+        else
+        {
+            buyButton.enabled = true;
         }
     }
 }
